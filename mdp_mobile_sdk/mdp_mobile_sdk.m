ab929273b02c8126b3741c869e674e4d98664d5f
Tim Vandecasteele <tim.vandecasteele@vasco.com>
2013-11-29 10:27:16 +0100

    Initial Commit

---
+++ mdp_mobile_sdk/mdp_mobile_sdk.m
@@ -0,0 +1,13 @@
+//
+//  mdp_mobile_sdk.m
+//  mdp_mobile_sdk
+//
+//  Created by Tim Vandecasteele on 29/11/13.
+//  Copyright (c) 2013 Vasco. All rights reserved.
+//
+
+#import "mdp_mobile_sdk.h"
+
+@implementation mdp_mobile_sdk
+
+@end
---
+++ mdp_mobile_sdkTests/mdp_mobile_sdkTests.m
@@ -0,0 +1,34 @@
+//
+//  mdp_mobile_sdkTests.m
+//  mdp_mobile_sdkTests
+//
+//  Created by Tim Vandecasteele on 29/11/13.
+//  Copyright (c) 2013 Vasco. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface mdp_mobile_sdkTests : XCTestCase
+
+@end
+
+@implementation mdp_mobile_sdkTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
